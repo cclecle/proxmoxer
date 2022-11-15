ac28c83fe09b7f89785b8f404bdf768241a47a0a
ChaCha <15073640+cclecle@users.noreply.github.com>
2022-11-15 11:42:59 +0000

    Update __init__.py

--- proxmoxer/__init__.py
+++ proxmoxer/__init__.py
@@ -1,6 +1,6 @@
 __author__ = "Oleg Butovich"
 __copyright__ = "(c) Oleg Butovich 2013-2017"
-__version__ = "1.3.x-DEV-PATCHPROXMOXUPLOAD"
+__version__ = "1.3.0+DEV-PATCHPROXMOXUPLOAD"
 __license__ = "MIT"
 
 from .core import *  # noqa
